a58cf4ae214a49c2309afd0b271b9a9e8a5f64b3
sonintact <hktts474941@gmail.com>
2024-08-29 18:02:29 +0700

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import TgpetIndex from "@/components/TgpetIndex";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import WebApp from "@twa-dev/sdk";
 
 export const metadata: Metadata = {
   title:
     "Ananlysts | TgpetAdmin",
   description: "",
 };
+
+WebApp.ready();
+WebApp.expand();
 
 export default function Home() {
   return (
