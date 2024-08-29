4dd5b2ce4e7af9add603d0b4f61c6f91b54fc5fe
sonintact <hktts474941@gmail.com>
2024-08-29 18:07:14 +0700

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import TgpetIndex from "@/components/TgpetIndex";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import TelegramWebAppInitializer from "./init";
 
 export const metadata: Metadata = {
   title:
@@ -11,6 +12,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
+      <TelegramWebAppInitializer />
       <DefaultLayout>
         <TgpetIndex />
       </DefaultLayout>
